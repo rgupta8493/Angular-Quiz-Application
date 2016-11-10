cc09a025cbdb51253c21a9db5c6b95b8acc74586
rgupta8493 <r.gupta8493@ufl.edu>
2016-11-10 03:02:58 -0500

    added customs buttons for Q&A

--- js/Controllers/quiz.js
+++ js/Controllers/quiz.js
@@ -4,6 +4,7 @@ QuizController.$inject = ['quizMetrics','DataService'];
 function QuizController(quizMetrics,DataService) {
     var vm = this;
     vm.quizMetrics = quizMetrics;
+    vm.dataService= DataService;
 }
 
 
