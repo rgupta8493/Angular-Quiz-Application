fc03f8151b64c4bbb8473d1c569ea4435118a9de
rgupta8493 <r.gupta8493@ufl.edu>
2016-11-10 03:47:11 -0500

    checking unanswered questions logic

--- js/Controllers/quiz.js
+++ js/Controllers/quiz.js
@@ -7,10 +7,34 @@ function QuizController(quizMetrics,DataService) {
     vm.dataService= DataService;
     vm.questionAnswered=questionAnswered;
     vm.activeQuestion =0;
-    
+    vm.setActiveQuestion= setActiveQuestion;
+    var numQuestionsAnswered =0;
+
+    function setActiveQuestion(){
+        var breakOut = false;
+        var quizLength= DataService.quizQuestions.length - 1;
+        
+        while(!breakOut){
+            vm.activeQuestion = vm.activeQuestion < quizLength ? ++vm.activeQuestion:0;
+            if(DataService.quizQuestions[vm.activeQuestion].selected===null){
+                breakOut = true;
+            }
+        }
+        
+        
+        
+    }
     
     function questionAnswered(){
-        
+        var quizLength = DataService.quizQuestions.length;
+        if(DataService.quizQuestions[vm.activeQuestion].
+          selected!==null){
+            numQuestionsAnswered++;
+            if(numQuestionsAnswered >= quizLength){
+                //finalise quiz
+            }
+        }
+        vm.setActiveQuestion();
         
         
     }
