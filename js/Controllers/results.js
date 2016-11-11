db0f6cb528902e0854bc1988b8f5fb2108201bac
rgupta8493 <r.gupta8493@ufl.edu>
2016-11-10 23:24:26 -0500

    adding btn danger to wrong and btn success to right answer

--- js/Controllers/results.js
+++ js/Controllers/results.js
@@ -7,4 +7,15 @@ function ResultsController(quizMetrics ,DataService){
     
      vm.quizMetrics =quizMetrics;
      vm.dataService = DataService;
+     vm.getAnswerClass= getAnswerClass;
+     vm.activeQuestion =0;
+     
+     function getAnswerClass(index){
+          if(index===quizMetrics.correctAnswers[vm.activeQuestion]){
+               return "bg-success"
+          }else if(index===DataService.quizQuestions[vm.activeQuestion].selected){
+              return "bg-danger";  
+          }
+          
+     }
  }
