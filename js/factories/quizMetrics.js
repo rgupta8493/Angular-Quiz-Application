31cd1d5ea5d20803419b304d5293a1b0df197a2a
rgupta8493 <r.gupta8493@ufl.edu>
2016-11-10 22:13:46 -0500

    marking quiz Questions

--- js/factories/quizMetrics.js
+++ js/factories/quizMetrics.js
@@ -1,12 +1,15 @@
 angular.module("myapp")
        .factory("quizMetrics", QuizMetrics);
-
-  function QuizMetrics(){
+  QuizMetrics.$inject =['DataService'];
+  function QuizMetrics(DataService){
     
      var quizObj ={
          quizActive: false,
          resultsActive : false,
          changeState: changeState
+         correctAnswers:[],
+         markQuiz: markQuiz,
+         numCorrect:0
          
      };  
       
@@ -21,6 +24,19 @@ angular.module("myapp")
           }else{
               return false;
           }
+      }
+      
+      function markQuiz(){
+          quizObj.correctAnswers = DataService.correctAnswers;
+          for(var i=0 ; i< DataService.quizQuestions.length;i++){
+              if(DataService.quizQuestions[i].selected ===DataService.correctAnswers[i]){
+                  DataService.quizQuestions[i].correct=true;
+                  quizObj.numCorrect++;
+              }else{
+                  DataService.quizQuestions[i].correct = false;
+              }
+          }
+          
       }
         
   }
